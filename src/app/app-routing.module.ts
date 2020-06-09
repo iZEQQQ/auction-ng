0427c9192bc09bbbf59017c1b1c611ec573cf965
jgorny <iceziro@wp.pl>
2020-06-09 20:03:17 +0200

    added a button with navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {BranchListViewComponent} from "./branch-list-view/branch-list-view.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'branches',
+    component: BranchListViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
